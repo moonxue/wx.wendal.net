26209436b586cb4dace73e9b08d2d7c9c4c2db81
Wendal Chen <wendal1985@gmail.com>
2013-04-02 13:51:03 +0800

    ...

--- main.go
+++ main.go
@@ -13,11 +13,21 @@ type WendalWeixinHandler struct {
 func (w *WendalWeixinHandler) Text(msg goweixin.Message) (reply goweixin.Replay) {
 	reply = goweixin.Replay{}
 	reply.SetContent("OK")
-	return reply
+	return
+}
+
+func (w *WendalWeixinHandler) Location(msg goweixin.Message) (reply goweixin.Replay) {
+	reply = goweixin.Replay{}
+	log.Println("Mark : " + msg.Label())
+	reply.SetContent("Mark : " + msg.Label())
+	return
 }
 
 func main() {
+	log.Fatal(http.ListenAndServe(":8883", &goweixin.WxHttpHandler{"", &WendalWeixinHandler{}}))
+}
+
+func init() {
 	goweixin.DevMode = true
 	goweixin.SetDebug(true)
-	log.Fatal(http.ListenAndServe(":8883", &goweixin.WxHttpHandler{"", &WendalWeixinHandler{}}))
 }
